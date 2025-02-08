cd0679787551215a1908233259a193cac2df93cb
Arman <contact.armankar@gmail.com>
2025-02-08 21:39:16 +0530

    completed env assignment

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const { resolve } = require('path');
+const config = require('./config');
 
 const app = express();
-const port = 3010;
+const port = process.env.PORT || 3010;
 
 app.use(express.static('static'));
 
 app.get('/', (req, res) => {
-  res.sendFile(resolve(__dirname, 'pages/index.html'));
+    res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Server running at http://localhost:${port}`);
+    console.log('Configuration:', config);
 });
